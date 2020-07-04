216de669c26ef4d0218b874ee45f24d56cce5b41
DESKTOP-EMDCG3P\Arelis-Lenovo <suaresramirez.a.j@gmail.com>
2020-07-03 22:17:35 -0300

    Método tipoTriangulo,

---
+++ src/practico4_p2_ej4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class practico4_p2_ej4 {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Ingrese el lado1");
+        int lado1 = input.nextInt();
+
+
+        System.out.println("Ingrese el lado2");
+        int lado2 = input.nextInt();
+
+
+        System.out.println("Ingrese el lado3");
+        int lado3 = input.nextInt();
+
+        System.out.println("La suma de los números pares entre 1 y el número ingresado es de " + sumaNumPares(num));
+
+    }
+}
