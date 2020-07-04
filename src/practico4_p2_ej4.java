6b0e5200a9a511146ddbda35e91804b9c9e11533
DESKTOP-EMDCG3P\Arelis-Lenovo <suaresramirez.a.j@gmail.com>
2020-07-03 22:41:15 -0300

    Método tipoTriangulo.

--- src/practico4_p2_ej4.java
+++ src/practico4_p2_ej4.java
@@ -7,15 +7,23 @@ public class practico4_p2_ej4 {
         System.out.println("Ingrese el lado1");
         int lado1 = input.nextInt();
 
-
         System.out.println("Ingrese el lado2");
         int lado2 = input.nextInt();
-
 
         System.out.println("Ingrese el lado3");
         int lado3 = input.nextInt();
 
-        System.out.println("La suma de los números pares entre 1 y el número ingresado es de " + sumaNumPares(num));
+        tipoTriangulo(lado1, lado2, lado3);
+    }
+
+    public static void tipoTriangulo(int lado1, int lado2, int lado3) {
+        if (lado1 == lado2 && lado2 == lado3) {
+            System.out.println("Es Equilatero");
+        } else if (lado1 != lado2 && lado2 != lado3 && lado1 != lado2) {
+            System.out.println("Es Escaleno");
+        } else{
+            System.out.println("Es Isosceles");
+        }
 
     }
 }
