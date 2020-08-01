c0e5133a855061dd2e54b8c922a209216f522ced
DESKTOP-EMDCG3P\Arelis-Lenovo <suaresramirez.a.j@gmail.com>
2020-08-01 01:04:33 -0300

    p8

--- src/main/java/Constant.java
+++ src/main/java/Constant.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
+public class Constants {
 
-public class Constant {
+    public final static int VALOR_IVA_2020 = 23;
+    public final static double VALOR_PI = 3.14;
+    public final static int AÑO_ACTUAL = 2020;
+
 }
--- src/main/java/Developer.java
+++ src/main/java/Developer.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+public class Developer extends Empleado{
 
-public class Developer {
+    private String lenguaje;
+
+    public Developer(String unNombre, int unTelefono, float unSueldo, String unLenguaje){
+        super(unNombre, unTelefono, unSueldo);
+        this.lenguaje = unLenguaje;
+    }
+
+    public String getLenguaje(){
+        return this.lenguaje;
+    }
+
+    public void setLenguaje(String unLenguaje){
+        this.lenguaje = unLenguaje;
+    }
+
+    public String toString(){
+
+        return super.toString() + " Su lenguaje es " + this.getLenguaje();
+
+    }
+
+
 }
